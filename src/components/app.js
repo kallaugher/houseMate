360a3562aebc2f3470e4aa826b84b1439bf76b03
jasmin47 <alxsanborn@gmail.com>
2016-09-30 16:40:32 -0400

    add basic components

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './nav_bar'
+import { Link } from 'react-router'
+
+export default (props) => {
+  return (
+    <div>
+      <NavBar title="Unnamed" url='/' />
+      <h1> Blah </h1>
+    </div>
+  )
+}
